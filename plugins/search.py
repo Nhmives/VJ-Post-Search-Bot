8e22546660f4e30f459e70e09238d7a06264b5bf
Nhmives <nhmovies9@gmail.com>
2025-03-03 22:55:37 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -41,7 +41,7 @@ async def search(bot, message):
     if message.text.startswith("/"):
        return    
     query   = message.text 
-    head    = f"<u>⭕ Here is the results {message.from_user.mention} 👇\n\n💢 Powered By </u> <b><I>@Nh_movies ❗</I></b>\n\n"
+    head    = f"<u>⭕ Here is the results {message.from_user.mention} 👇\n\n💢 Powered By </u> <b><I>@Nh_backup ❗</I></b>\n\n"
     results = ""
     try:
        for channel in channels:
@@ -84,7 +84,7 @@ async def recheck(bot, update):
     id      = update.data.split("_")[-1]
     query   = await search_imdb(id)
     channels = (await get_group(update.message.chat.id))["channels"]
-    head    = "<u>⭕ I Have Searched Movie With Wrong Spelling But Take care next time 👇\n\n💢 Powered By </u> <b><I> ❗</I></b>\n\n"
+    head    = "<u>⭕ I Have Searched Movie With Wrong Spelling But Take care next time 👇\n\n💢 Powered By </u> <b><I>@Nh_backup ❗</I></b>\n\n"
     results = ""
     try:
        for channel in channels:
