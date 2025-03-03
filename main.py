c8afdb3982882608787b44793150c2767616209d
Nhmives <nhmovies9@gmail.com>
2025-03-04 00:33:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,7 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from pyrogram import Client
-import os
+from client import Bot
 
-app = Client(
-    "bot",
-    api_id=os.getenv("API_ID"),
-    api_hash=os.getenv("API_HASH"),
-    bot_token=os.getenv("BOT_TOKEN"),
-    plugins=dict(root="plugins")  # Loads all plugins in the `plugins` folder
-)
-
-app.run()
+print("Bot Started 👍 Powered By @insightweav")
+Bot().run()
